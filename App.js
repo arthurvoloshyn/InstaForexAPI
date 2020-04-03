e057ef82d0081417f86053135e91a3eba16b5531
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-04-03 20:13:29 +0300

    Version 1

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Instruments from "./src/Instruments";
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <Instruments />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
---
+++ src/Instrument/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Instrument = ({ title }) => {
+    return (
+        <View>
+            <Text>{title}</Text>
+        </View>
+    );
+};
+
+export default Instrument;
