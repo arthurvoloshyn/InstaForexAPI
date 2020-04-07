9fef0764af1d86042ef316304399419b89fbd670
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-04-07 17:20:48 +0300

    Add fonts

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { View } from 'react-native';
+import { useFonts } from '@use-expo/font';
+import { AppLoading } from 'expo';
 import { QuotesProvider } from './src/context/quotesContext';
 import QuotesList from "./src/components/QuotesList";
 import ErrorBoundary from "./src/components/ErrorBoundary";
 
-const App = () => (
-    <View>
-        <ErrorBoundary>
-            <QuotesProvider>
-                <QuotesList />
-            </QuotesProvider>
-        </ErrorBoundary>
-    </View>
-);
+const App = () => {
+    const [fontsLoaded] = useFonts({
+        'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf'),
+        'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf'),
+    });
+
+    if (!fontsLoaded) return <AppLoading />;
+
+    return (
+        <View>
+            <ErrorBoundary>
+                <QuotesProvider>
+                    <QuotesList/>
+                </QuotesProvider>
+            </ErrorBoundary>
+        </View>
+    );
+};
 
 export default App;
