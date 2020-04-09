802ba69bf019839609f1084174838d2035b4f9e1
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-04-09 17:33:33 +0300

    Add propTypes to NavBar component and edit styles for it

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View } from 'react-native';
+import PropTypes from 'prop-types';
+import AppText from '../AppText';
+import styles from './styles';
+
+const NavBar = ({ title }) => (
+    <View style={styles.navbar}>
+        <AppText style={styles.text}>{title}</AppText>
+    </View>
+);
+
+NavBar.propTypes = {
+    title: PropTypes.string,
+};
+
+NavBar.defaultProps = {
+    title: 'InstaForex',
+};
+
+export default NavBar;
+
+
---
+++ src/components/NavBar/styles.js
@@ -0,0 +1,28 @@
+import { Platform, StyleSheet } from "react-native";
+import { PRIMARY_COLOR } from "../../constants/themes";
+import { isIOS } from "../../services/detectDevice";
+
+const styles = StyleSheet.create({
+    navbar: {
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        paddingBottom: 10,
+        ...Platform.select({
+            ios: {
+                borderBottomColor: PRIMARY_COLOR,
+                borderBottomWidth: 1,
+            },
+            android: {
+                backgroundColor: PRIMARY_COLOR,
+            },
+        }),
+    },
+    text: {
+        color: isIOS ? PRIMARY_COLOR : '#fff',
+        fontSize: 20,
+        fontWeight: 800,
+    },
+});
+
+export default styles;
