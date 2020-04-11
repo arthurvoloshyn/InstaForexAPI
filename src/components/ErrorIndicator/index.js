710475c5370f56e86cb19e7b7bb026622179f504
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-04-11 12:09:28 +0300

    Edit ErrorIndicator

--- src/components/ErrorIndicator/index.js
+++ src/components/ErrorIndicator/index.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import { View } from 'react-native';
 import PropTypes from "prop-types";
-import { DANGER_COLOR } from "../../constants/themes";
-import AppButton from "../AppButton";
 import AppText from "../AppText";
 import styles from './styles';
 
-const ErrorIndicator = ({ onPress }) => (
+const ErrorIndicator = ({ children }) => (
     <View style={styles.center}>
         <AppText textStyle={styles.error}>Something went wrong!</AppText>
-        <AppButton onPress={onPress} backgroundColor={DANGER_COLOR}>Try again</AppButton>
+        {children}
     </View>
 );
 
 ErrorIndicator.propTypes = {
-    onPress: PropTypes.func,
+    children: PropTypes.node,
 };
 
 ErrorIndicator.defaultProps = {
-    onPress: () => {},
+    children: null,
 };
 
 export default ErrorIndicator;
