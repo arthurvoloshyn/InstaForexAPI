090151fb583c5010a982f921e5a24fec71f8480a
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-04-16 20:09:13 +0300

    edit paginate method in usePagination hook

--- src/hooks/usePagination/index.js
+++ src/hooks/usePagination/index.js
@@ -5,11 +5,8 @@ const usePagination = initialPage => {
   const [currentPage, setCurrentPage] = useState(initialPage);
 
   const paginate = operator => {
-    if (operator === NEXT) {
-      setCurrentPage(currentPage + 1);
-    } else if (operator === PREV) {
-      setCurrentPage(currentPage - 1);
-    }
+    operator === NEXT && setCurrentPage(currentPage + 1);
+    operator === PREV && setCurrentPage(currentPage - 1);
   };
 
   return [currentPage, paginate];
