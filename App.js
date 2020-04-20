b87dd4774d58c02fee6107cec4bfd437f280b852
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-04-20 16:15:04 +0300

    Edit eslint path

--- App.js
+++ App.js
@@ -3,26 +3,26 @@ import React from 'react';
 import { useFonts } from '@use-expo/font';
 import { AppLoading } from 'expo';
 import MainProvider from './src/contextes/mainContext';
-import ErrorBoundary from "./src/components/ErrorBoundary";
-import Main from "./src/layouts/Main";
+import Main from './src/layouts/Main';
+import ErrorBoundary from './src/components/ErrorBoundary';
 import robotoRegularFont from './assets/fonts/Roboto-Regular.ttf';
 import robotoBoldFont from './assets/fonts/Roboto-Bold.ttf';
 
 const App = () => {
-    const [fontsLoaded] = useFonts({
-        'roboto-regular': robotoRegularFont,
-        'roboto-bold': robotoBoldFont,
-    });
+  const [fontsLoaded] = useFonts({
+    'roboto-regular': robotoRegularFont,
+    'roboto-bold': robotoBoldFont,
+  });
 
-    if (!fontsLoaded) return <AppLoading />;
+  if (!fontsLoaded) return <AppLoading />;
 
-    return (
-        <ErrorBoundary>
-            <MainProvider>
-                <Main />
-            </MainProvider>
-        </ErrorBoundary>
-    );
+  return (
+    <ErrorBoundary>
+      <MainProvider>
+        <Main />
+      </MainProvider>
+    </ErrorBoundary>
+  );
 };
 
 export default App;
