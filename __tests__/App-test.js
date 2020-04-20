4540374585d20dc28a1f8c98f0f0814e5ac1fffe
Arthur Voloshyn <grandfaraon93@gmail.com>
2020-04-20 17:29:43 +0300

    Add testing for project

---
+++ __tests__/App-test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import QuotesList from '../src/screens/QuotesList';
+
+describe('App', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<QuotesList />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
